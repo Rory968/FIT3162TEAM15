e7a2374366308219a3977107225fcf4ca9404b86
Rory Austin <roryaustin98@gmail.com>
2019-08-19 11:59:02 +1000

    Week 3 commit 1.
    This commit changes the split-data file
    
    A number of functions were added that correspond to different
    under and over sampling methods. These are used in the block
    underneath the functions.
    
    Also the plot_2d function was altered to operate in two
    dimensions corresponding to occurence data that we are using
    for the point observations.

--- split_data.py
+++ split_data.py
@@ -1,10 +1,19 @@
 # Written by Rory Austin id: 28747194
 import pandas as pd
 import numpy as np
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import TomekLinks, RandomUnderSampler
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 import read_database as read
+
+'''
+This file contains all functions and data types relevant to the over-sampling work needed to manipulate the
+point data. The block below runs through the process used. All of the functions below this comment block are
+used to make that process happen.
+'''
 
 
 def clean_data(data_frame, split_attr):
@@ -63,24 +72,107 @@ def plot_2d(x, y, label='Classes'):
     :param label: label for the plot.
     :return: blank.
     '''
-    colours = ['#1F77B4', '#FF7F0E', 'g']
-    markers = ['o', 's', 'v']
-    for l, c, m in zip(np.unique(y), colours, markers):
-        plt.scatter(x[y == l, 0],
-                    x[y == l, 1 or 3],
-                    x[y == l, 2 or 4],
-                    c=c, label=l, marker=m)
+    pca = PCA(n_components=2)
+    x_new = pca.fit_transform(x)
+    two_colour = ['#1F77B4', '#FF7F0E']
+    two_marker = ['o', 's']
+    for l, c, m in zip(np.unique(y), two_colour, two_marker):
+        plt.scatter(x_new[y == l, 0],
+                    x_new[y == l, 1 or 3],
+                    c=c, linewidth=0.5, label=l, marker=m)
 
     plt.title(label)
     plt.legend(loc='upper right')
     plt.show()
 
 
+def oversampleSMOTE(x, y):
+    '''
+    This function over-samples the data using SMOTE. This results in a higher
+    concentration of minority class points. In our case we will be using occurence data so
+    the minority class are the data points that are close to range.
+
+    :param x: data without labels.
+    :param y: corresponding labels.
+    :return: returns an oversampled numpy array and labels.
+    '''
+    smote = SMOTE(ratio='minority')
+    x_sm, y_sm = smote.fit_sample(x, y)
+    # print(x.shape[0], x_sm.shape[0])
+    print(x_sm.shape[0] - x.shape[0], 'new synthetic points')
+    plot_2d(x_sm, y_sm)
+    return x_sm, y_sm
+
+
+def random_over(x, y):
+    '''
+    This function over-samples the data randomly, this means creating duplicates.
+    Not as effective as SMOTE but is used on the majority class which is more useful
+    in terms of occurence data sets.
+
+    :param x: data without labels.
+    :param y: corresponding labels.
+    :return: returns an oversampled numpy array and labels.
+    '''
+    ros = RandomOverSampler()
+    x_ros, y_ros = ros.fit_sample(x, y)
+    # print(x.size, x_ros.size)
+    print(x_ros.shape[0] - x.shape[0], 'new random picked points')
+    plot_2d(x_ros, y_ros, 'ROS')
+    return x_ros, y_ros
+
+
+def tomek_under(x, y):
+    '''
+    This function uses tomek links to under-sample the data set,
+    Only balances slightly, doesnt really accoutn for the minority class.
+
+    :param x: data without labels.
+    :param y: corresponding labels.
+    :return: returns an under-sampled numpy array and labels.
+    '''
+    tl = TomekLinks(return_indices=True, ratio='majority')
+    x_tl, y_tl, id_tl = tl.fit_sample(x, y)
+    print('Removed Indexses: ', id_tl)
+    plot_2d(x_tl, y_tl, 'Tomek Under-sampling')
+    return x_tl, y_tl
+
+
+def random_under(x, y):
+    '''
+    This function uses random under-sampling to balance the majority
+    set with the minority set. This does well to balance but as a result a lot of data is lost.
+
+    :param x: data without labels.
+    :param y: corresponding labels.
+    :return: returns under-sampled numpy array and labels.
+    '''
+    rus = RandomUnderSampler(return_indices=True)
+    x_rus, y_rus, id_rus = rus.fit_sample(x, y)
+    print('Removed indexes: ', id_rus)
+    plot_2d(x_rus, y_rus, 'Random under-sampling')
+    return x_rus, y_rus
+
+
+
+'''
+This block of code splits and cleans the data and then oversamples the data and returns the corresponding data and its labels.
+There are a number of different methods for doing this, there are SMOTE and Random over-sampling aswell as Tomek-links and 
+Random under-sampling.
+Currently the random under-sampling is running but all options are there to be commented and uncommented in future.
+
+This currently has base functionality and does not write to the data-base. These features will be added later according to plan.
+'''
+
 data_frame = read.read_collection('Species_DB', 'Antechinus_agilis')
 clean = clean_data(data_frame, 'RATING_INT')
 # print(data_frame['SCIENTIFIC_DISPLAY_NME'].head())
-x, y = create_training_set(clean, 'RATING_INT')
-pca = PCA(n_components=3)
-x_new = pca.fit_transform(x)
-# print(acc, un)
-plot_2d(x_new, y, 'Imbalanced dataset (3 PCA components)')
+x, y = create_training_set(clean[['LATITUDEDD_NUM', 'LONGITUDEDD_NUM', 'RATING_INT']], 'RATING_INT')
+plot_2d(x, y, 'Imbalanced dataset (2 PCA components)')
+# sm = oversampleSMOTE(x, y)[0]
+# ros = random_over(x, y)[0]
+# tl = tomek_under(x, y)[0]
+rus = random_under(x, y)[0]
+# print(rus.shape[0], rus.tolist())
+# print(tl.shape[0], tl.tolist())
+
